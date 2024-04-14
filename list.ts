5766a15ea2167db3860f2c62380c64862642dcea
Bisma Qazi <bismaqazi2005@gmail.com>
2024-04-14 23:24:33 +0500

    first commit

---
+++ list.ts
@@ -0,0 +1,25 @@
+#! usr/bin
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true
+while(condition)
+{
+
+let addTask = await inquirer.prompt([{
+    name: "todo",
+    type: "input",
+    message: "What do you need to do?"
+
+},
+{
+    name: "add",
+    type: "list",
+    message: "Do you want to add more tasks?",
+    choices: ["Yes", "No"]
+}
+]);
+todos.push(addTask.todo);
+condition = addTask.add
+console.log(todos)
+}
